ef1e67c83caf812b5898849613085b03ab3b6675
rich sollenne <rich@sollenne.com>
2014-02-07 20:38:41 -0600

    commit minor change

--- application/views/locator/viewUnit.php
+++ application/views/locator/viewUnit.php
@@ -6,7 +6,7 @@
 	<a class="btn pull-right" href="javascript: void()" onclick="document.getElementById('message').submit()"><i class="icon-comments-alt"></i> Send Message</a>
 </form>
 <ul>
-	<li>Date Available: <?php echo $unit->date_available;?></li>
+	<li>Available: <?php echo $unit->date_available;?></li>
 	<li>Bedrooms: <?php echo $unit->beds;?></li>
 	<li>Baths: <?php echo $unit->baths;?></li>
 	<li>Size: <?php echo $unit->size;?> sq.ft.</li>
@@ -14,8 +14,8 @@
 	<li>Furnished: <?php if($unit->furnished){echo 'yes';} else {echo 'no';}?></li>
 	<li>Washer/Dryer: <?php echo $unit->washer;?></li>
 	<li>Direction Facing: <?php echo $unit->direction;?></li>
-	<li>Requirements:<br/><?php if(!property_exists($unit, 'requirements') || $unit->requirements == null || $unit->requirements == ''){echo 'none';} else {echo $unit->requirements;}?></li>
-	<li>Specials:<br/><?php if(!property_exists($unit, 'specials') || $unit->specials == null || $unit->specials == ''){echo 'none';} else {echo $unit->specials;}?></li>
+	<li>Requirements: <?php if(!property_exists($unit, 'requirements') || $unit->requirements == null || $unit->requirements == ''){echo 'none';} else {echo $unit->requirements;}?></li>
+	<li>Specials: <?php if(!property_exists($unit, 'specials') || $unit->specials == null || $unit->specials == ''){echo 'none';} else {echo $unit->specials;}?></li>
 	<li>Commission: <?php if(!property_exists($unit, 'commission') || $unit->commission == null || $unit->commission == ''){echo 'none';} else {echo $unit->commission;}?></li>
 </ul>
 
